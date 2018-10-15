20f2df8ae6dd21f21494692f8a6cc7ff7eed608f
Colin Campbell <colincampbell@colins-mbp.home>
2018-10-15 22:18:34 +0100

    updated jenkins.tf

--- terraform/jenkins.tf
+++ terraform/jenkins.tf
@@ -1,7 +1,10 @@
 module "jenkins" {
-  source        = "/modules/tf-aws-ec2/"
-  ami           = "RHEL-7.5_HVM_GA-*-x86_64-1-Hourly2-GP2"
-  name          = "jenkins"
-  instance_type = "t2.medium"
-  asg_size      = "1"
+  source          = "modules/tf-aws-asg/"
+  ami             = "RHEL-7.5_HVM_GA-*-x86_64-1-Hourly2-GP2"
+  name            = "jenkins"
+  instance_type   = "t2.medium"
+  asg_size        = "1"
+  key_name        = "colinmac"
+  vpc_zone_id     = [""]
+  security_groups = ["${aws_security_group.jenkins_sg.id}"]
 }
